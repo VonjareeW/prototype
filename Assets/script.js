ff909e2e6b2b331fdaa027309120dbeed095ddde
VonjareeW <52430595+VonjareeW@users.noreply.github.com>
2023-08-12 21:35:42 -0400

    Update script.js

--- Assets/script.js
+++ Assets/script.js
@@ -1,20 +1,32 @@
 // Assignment code here
-var name=""
-function greeting(){
-  var name=prompt("What is your name ?");
- 
- alert("Welcome to this website " + name);
+// Declare the variable 'name' outside the function to make it accessible globally
+var name = "";
+
+// Function to greet the user
+function greeting() {
+  // Use the global 'name' variable to store the user's name
+  var name = prompt("What is your name?");
+
+  // Use template literals for string interpolation
+  alert(`Welcome to this website, ${name}! Please Have a wonderful day full of joy!`);
 }
-greeting();
+
+// Call the greeting function twice
 greeting();
 
-function amountdue(price, quantity){
-  var price=prompt("What is the price ");
-  var quantity=prompt("What is the quantity");
-  var subtotal=price * quantity;
+
+// Function to calculate the amount due
+function amountdue(price, quantity) {
+  // Remove the local 'price' and 'quantity' variables as they are unnecessary
+  // The function parameters already serve this purpose
+  var subtotal = price * quantity;
   var tax = 1.05;
-  var total =subtotal * tax;
+  var total = subtotal * tax;
   return total;
 }
 
-alert("The amount due is $ " + amountdue(10,3));
+// Call the amountdue function with specified arguments and display the result
+var priceInput = parseFloat(prompt("What is the price?"));
+var quantityInput = parseInt(prompt("What is the quantity?"), 10);
+var totalAmount = amountdue(priceInput, quantityInput);
+alert(`The amount due is $${totalAmount.toFixed(2)}`);
